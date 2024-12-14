81ae3644041dd4918d99ffe7bf119778a27f7a89
Mohammad Ali Reza <ali.reza.l33t@gmail.com>
2024-12-14 14:56:29 +0000

    .

--- settings.py
+++ settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from oscar.defaults import *    
+from oscar.defaults import * 
+import os   
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
