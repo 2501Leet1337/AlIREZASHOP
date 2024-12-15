47e37f50c31eaee5d6aa97a04ac27ec515511d10
Mohammad Ali Reza <ali.reza.l33t@gmail.com>
2024-12-15 13:05:52 +0000

    .

--- middleware.py
+++ middleware.py
@@ -27,3 +27,11 @@ class ThrottleMiddleware:
 
         return self.get_response(request)
 
+    def get_client_ip(self, request):
+        """Retrieve client IP address."""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
